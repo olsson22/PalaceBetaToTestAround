6a3aa557f0a42b8a8c8c941e7b2026acc37cf909
Meredith Marcinko <marcinko21@up.edu>
2019-11-05 09:30:10 -0800

    made the Testing Class for testing methods, also made the testing methods for each method (nothing is written in any of them)

---
+++ app/src/test/java/com/example/palacealpha01/PalaceTest.java
@@ -0,0 +1,73 @@
+package com.example.palacealpha01;
+
+import org.junit.Test;
+
+
+/**
+ * Tests the Methods in the PalaceGameState
+ *
+ * @author  Andres, Maximilian, Fredrik, Meredith
+ * @version 5 November 2019
+ */
+public class PalaceTest
+{
+    @Test
+    public void shuffleTheDeckTest()
+    {
+
+    }
+
+    @Test
+    public void selectCardsTest()
+    {
+
+    }
+
+    @Test
+    public void selectPalaceCardsTest()
+    {
+
+    }
+
+    @Test
+    public void playCardsTest()
+    {
+
+    }
+
+    @Test
+    public void changePalaceTest()
+    {
+
+    }
+
+    @Test
+    public void confirmPalaceTest()
+    {
+
+    }
+
+    @Test
+    public void takeDiscardPilTest()
+    {
+
+    }
+
+    @Test
+    public void dealTheDeckTest()
+    {
+
+    }
+
+    @Test
+    public void bombDiscardPileTest()
+    {
+
+    }
+
+    @Test
+    public void isDrawPileEmptyTest()
+    {
+
+    }
+}

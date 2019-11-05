b221af0c6c0c904e0d5260197e7a83fbfac62071
Meredith Marcinko <marcinko21@up.edu>
2019-11-05 08:41:08 -0800

    Player 2's hand is now not visible to the player

--- app/src/main/java/com/example/palacealpha01/GameFramework/palace/PalaceSurfaceView.java
+++ app/src/main/java/com/example/palacealpha01/GameFramework/palace/PalaceSurfaceView.java
@@ -151,6 +151,7 @@ public class PalaceSurfaceView extends SurfaceView implements View.OnTouchListen
         for (Pair p : pgs.the_deck) {
             if (p.get_location() == Location.PLAYER_TWO_HAND) {
                 canvas.drawBitmap(cardBack, xP2H, yP2H, bitmapPaint);
+                //
                 xP2H += cardWidth;
             }
         }

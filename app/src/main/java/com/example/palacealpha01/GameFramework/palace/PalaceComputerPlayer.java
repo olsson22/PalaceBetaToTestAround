996d3853874750e5b8359c470f16bb8155cbc350
Maximus7410 <33791590+Maximus7410@users.noreply.github.com>
2019-11-07 18:02:23 -0800

    Maximilian & Fredrik: We implemented Random AI functionality.

--- app/src/main/java/com/example/palacealpha01/GameFramework/palace/PalaceComputerPlayer.java
+++ app/src/main/java/com/example/palacealpha01/GameFramework/palace/PalaceComputerPlayer.java
@@ -4,6 +4,8 @@ import com.example.palacealpha01.GameFramework.GameComputerPlayer;
 import com.example.palacealpha01.GameFramework.infoMessage.GameInfo;
 import com.example.palacealpha01.GameFramework.infoMessage.NotYourTurnInfo;
 
+import java.util.ArrayList;
+
 public class PalaceComputerPlayer extends GameComputerPlayer
 {
 
@@ -17,45 +19,48 @@ public class PalaceComputerPlayer extends GameComputerPlayer
 	protected void receiveInfo(GameInfo info)
 	{
 		if (info instanceof NotYourTurnInfo)
-		{
 			return;
-		}
-		if(info instanceof PalaceGameState){
+
+		if (info instanceof PalaceGameState)
+		{
 			info.setGame(game);
 			PalaceGameState pgs = (PalaceGameState) info;
 
-			if(pgs.getTurn()==this.playerNum){
-				for(Pair p: pgs.getSelectedCards()) {
+			if (pgs.getTurn() == this.playerNum)
+			{
+//				sleep(2);
 
-				if (pgs.getSelectedCards().contains(p)){
-					PalacePlayCardAction playCardAction = new PalacePlayCardAction(this);
-					game.sendAction(playCardAction);
-
-					}
+				if (pgs.getSelectedCards().size() > 0)
+				{
+					game.sendAction(new PalacePlayCardAction(this));
+					return;
 				}
-			}
 
-			if(pgs.getTurn()==this.playerNum){
-				for(Pair pair: pgs.the_deck){
-					if(pair.get_location()== Location.PLAYER_TWO_HAND){
-						PalaceSelectCardAction selected = new PalaceSelectCardAction(this, pair);
-						game.sendAction(selected);
-						sleep(2);
-						return;
-					}
+				ArrayList<Pair> player_two_hand = new ArrayList<>();
+				for (Pair p : pgs.the_deck)
+					if (p.get_location() == Location.PLAYER_TWO_HAND)
+						player_two_hand.add(p);
 
+				if (player_two_hand.size() == 0)
+					for (Pair p : pgs.the_deck)
+						if (p.get_location() == Location.PLAYER_TWO_UPPER_PALACE)
+							player_two_hand.add(p);
+
+				if (player_two_hand.size() == 0)
+					for (Pair p : pgs.the_deck)
+						if (p.get_location() == Location.PLAYER_TWO_LOWER_PALACE)
+							player_two_hand.add(p);
+
+				if (player_two_hand.size() > 0)
+				{
+					Pair selected_pair = player_two_hand.get((int) (Math.random() * player_two_hand.size()));
+					game.sendAction(new PalaceSelectCardAction(this, selected_pair));
+					return;
 				}
-			}
 
+				game.sendAction(new PalaceTakeDiscardPileAction(this));
+			}
 		}
-
 		//TODO: add functions to make the computer player play cards and not only pick up the discard pile every time.
-
-		//	game.sendAction(new PalaceTakeDiscardPileAction(this));
-		//	sleep(2);
-
-
-
-
 	}
 }
